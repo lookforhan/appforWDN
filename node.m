8c85afa89b4c39f5e8af41eb74c797c700e59dce
han <767122454@qq.com>
2018-03-16 17:28:52 +0800

    a new class:node;do not finish

---
+++ node.m
@@ -0,0 +1,13 @@
+%% name: node.m
+% this is a class of node
+classdef node < handle
+    properties
+        id
+        x
+        y
+    end
+    methods
+        function node
+        end
+    end
+end
