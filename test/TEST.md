ccd670ff3c02ee4b30be5577f87b52bd34d36cc1
lookforhan <767122454@qq.com>
2018-03-26 21:48:48 +0800

    add test file

---
+++ test/TEST.md
@@ -0,0 +1,5 @@
+# test file
+##1.propose neccessary demand
+1 read INP file
+2 show the water distribution network model
+3 creat the break model
---
+++ test/appUnitTest.m
@@ -0,0 +1,17 @@
+% name appUnitTest
+classdef appUnitTest < matlab.unittest.TestCase
+    properties
+        model% the test property is object of app
+    end
+    methods(TestClassSetup)%run before the whole test
+    end
+    methods(TestClassTeardown)% run after the whole test
+    end
+    methods(TestMethodSetup)% run before each test
+    end
+    methods(TestMethodTeardown)% run after each test
+    end
+    methods(Test)% add test point
+    end
+end
+
